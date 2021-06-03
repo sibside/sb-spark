141d6395a97a21b18a99385a054df602ddf16c69
Denis <denisidorenko@gmail.com>
2021-06-03 22:58:08 +0700

    Update agg.scala

--- lab04b/agg/src/main/scala/agg.scala
+++ lab04b/agg/src/main/scala/agg.scala
@@ -1,5 +1,4 @@
-import org.apache.spark.sql.streaming.Trigger
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 /**
  * @author Sidorenko Denis
@@ -15,46 +14,32 @@ object agg extends App {
     .getOrCreate()
 
   val topic_name = "denis_sidorenko"
-
-  var offset = spark.conf.get("spark.filter.offset") // earliest
-  if (offset != "earliest") {
-    offset = s"""{"$topic_name":{"0":$offset}}"""
-  }
+  var offset = "earliest"
 
   // Параметры подключения и чтения из Кафки
   val kafkaParams = Map(
     "kafka.bootstrap.servers" -> "spark-master-1:6667",
     "subscribe" -> topic_name,
-    "startingOffsets" -> offset,
-    "endingOffsets" -> "latest"
+    "startingOffsets" -> offset
   )
 
   /*
    PART 2 - Read data from Kafka
   */
   // Читаем данные из топика name_surname
-  val dfIn = spark
-    .readStream
+  val dfIn = spark.readStream
     .format("kafka")
     .options(kafkaParams)
     .load
     .withWatermark("timestamp", "1 hours")
-    .dropDuplicates(Seq("uid", "timestamp"))
-
-  // Создаем синк для записи данных в Kafka
-  val kafkaSink = createKafkaSink("DSout4", dfIn)
 
   // Пишем данные в топик name_surname_lab04b_out
-  val sq_kafka = kafkaSink.start
-
-  // Создаем синк для записи в Kafka
-  def createKafkaSink(chkName: String, df: DataFrame) = {
-    df
-      .writeStream
-      .format("kafka")
-      .trigger(Trigger.ProcessingTime("10 seconds"))
-      .option("checkpointLocation", s"/tmp/chk/$chkName")
-      .option("truncate", "false")
-      .option("numRows", "20")
-  }
+  //  dfIn.selectExpr("CAST(uid AS STRING) AS key", "to_json(struct(*)) AS value")
+  dfIn.writeStream
+    .format("kafka")
+    .outputMode("append")
+    .option("kafka.bootstrap.servers", "spark-master-1:6667")
+    .option("topic", "denis_sidorenko_lab04b_out")
+    .start()
+    .awaitTermination()
 }
